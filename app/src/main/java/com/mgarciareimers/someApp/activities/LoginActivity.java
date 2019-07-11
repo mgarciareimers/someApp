2c1affdd01e787b633a4fa8c2814e58f6fa81060
miguelgarciareimers <miguel.garcia@magocode.com>
2019-07-11 10:14:55 +0200

    Login Activity Layout
    Login activity layout completed: Sign up link + Google login button + Facebook login button + Some styling have been added.
    
    Sign up activity has been created!

---
+++ app/src/main/java/com/mgarciareimers/someApp/activities/LoginActivity.java
@@ -0,0 +1,96 @@
+package com.mgarciareimers.someApp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.mgarciareimers.someApp.R;
+import com.mgarciareimers.someApp.commons.Utilities;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton, googleLoginButton, facebookLoginButton;
+    private EditText emailEditText, passwordEditText;
+    private TextView signUpQuestionTextView, signUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.defineFields();
+        this.defineFieldActions();
+    }
+
+    // Method that defines the fields.
+    private void defineFields() {
+        this.loginButton = this.findViewById(R.id.loginButton);
+        this.googleLoginButton = this.findViewById(R.id.googleLoginButton);
+        this.facebookLoginButton = this.findViewById(R.id.facebookLoginButton);
+        this.emailEditText = this.findViewById(R.id.emailEditText);
+        this.passwordEditText = this.findViewById(R.id.passwordEditText);
+        this.signUpQuestionTextView = this.findViewById(R.id.signUpQuestionTextView);
+        this.signUpTextView = this.findViewById(R.id.signUpTextView);
+    }
+
+    // Method that defines the actions of the fields.
+    private void defineFieldActions() {
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginButtonClicked();
+            }
+        });
+        this.googleLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { onGoogleLoginButtonClicked();
+            }
+        });
+        this.facebookLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { onFacebookLoginButtonClicked();
+            }
+        });
+        this.signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignUpClicked();
+            }
+        });
+        this.signUpQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { onSignUpClicked();
+            }
+        });
+    }
+
+    // Method that is called when the user clicks the login button.
+    private void onLoginButtonClicked() {
+        if (Utilities.emailIsValid(this.emailEditText.getText().toString()) && Utilities.passwordIsValid(this.passwordEditText.getText().toString())) {
+            Log.d("LoginButton", "Login...");
+        } else {
+            Log.d("LoginButton", "Not valid!");
+        }
+    }
+
+    // Method that is called when the user clicks the google login button.
+    private void onGoogleLoginButtonClicked() {
+        Log.d("GoogleLoginButton", "Google login...");
+    }
+
+    // Method that is called when the user clicked the facebook login button.
+    private void onFacebookLoginButtonClicked() {
+        Log.d("FacebookLoginButton", "Facebook login...");
+    }
+
+    // Method that is called when the user click the sign up texts.
+    private void onSignUpClicked() {
+        this.startActivity(new Intent(this, SignUpActivity.class));
+    }
+}
